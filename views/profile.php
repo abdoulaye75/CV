01bc7c92156af3d2342625ae623dc88e871d08cc
abdoulaye <abdoulayedabo@laposte.net>
2017-06-28 22:43:20 +0200

    style et responsive design des parties profil et compétences

--- views/profile.php
+++ views/profile.php
@@ -1,22 +1,31 @@
 <section id="profile">
 	<h2 class="job"> Développeur web junior </h2>
 	<p class="accroche"> Aptitude à travailler en équipe, force de proposition et respectueux des bonnes pratiques de travail. Sens du relationnel. </p>
-	<a href="file/cv.pdf" class="file"> <img src="img/icon_pdf.png" alt="icône du format pdf" width="100" height="100"> Télécharger mon <br> CV papier </a>
 
 	<div class="card">
-		<img src="img/abdoulaye.jpg" alt="ma photo">
+		<div class="images">
+			<img src="img/abdoulaye.jpg" alt="ma photo">
+			<a href="file/cv.pdf" class="file"> <img src="img/icon_pdf.png" alt="icône du format pdf" class="pdf"> Télécharger mon <br> CV papier </a>
+		</div>
 		<div class="infos">
-			<ul class="coordonnees">
-				<li> abdoulaye dabo </li>
-				<li> 28 ans </li>
-				<li> 156 rue des poissonniers </li>
-				<li> 75018 </li>
-				<li> paris </li>
-				<li> abdoulayedabo@laposte.net </li>
-				<li> 06.58.14.59.75 </li>
-				<li> <a href="https://github.com/abdoulaye75" target="_blank"> Lien github </a> </li>
-				<li> <a href="https://www.linkedin.com/in/abdoulaye-dabo/" target="_blank"> Lien linkedin </a> </li>
-			</ul>
+			<div>
+				<ul>
+					<li> abdoulaye dabo </li>
+					<li> 28 ans </li>
+					<li> 156 rue des poissonniers </li>
+					<li> 75018 </li>
+					<li> paris </li>
+				</ul>
+			</div>
+
+			<div>
+				<ul>
+					<li> abdoulayedabo@laposte.net </li>
+					<li> 06.58.14.59.75 </li>
+					<li> <a href="https://github.com/abdoulaye75" target="_blank"> Lien github </a> </li>
+					<li> <a href="https://www.linkedin.com/in/abdoulaye-dabo/" target="_blank"> Lien linkedin </a> </li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </section>
--- views/skills.php
+++ views/skills.php
@@ -1,22 +1,32 @@
 <section id="cv">
 	<h3> Compétences techniques : </h3>
-	<ul class="technical">
-		<li> html 5 <div class="myProgress"> <div class="level"> 90% </div> </div> </li>
-		<li> css 3 <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
-		<li> javascript <div class="myProgress"> <div class="level"> 75% </div> </div> </li>
-		<li> php 7 <div class="myProgress"> <div class="level"> 60% </div> </div> </li>
-		<li> git <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
-		<li> bootstrap <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
-		<li> sass <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
-		<li> jquery <div class="myProgress"> <div class="level"> 80% </div> </div> </li>
-		<li> angularjs <div class="myProgress"> <div class="level"> 70% </div> </div> </li>
-		<li> sql <div class="myProgress"> <div class="level"> 60% </div> </div> </li>
-	</ul>
+	<div class="skills">
+		<div class="languages">
+			<ul class="technical">
+				<li> html 5 <div class="myProgress"> <div class="level"> 90% </div> </div> </li>
+				<li> css 3 <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
+				<li> javascript <div class="myProgress"> <div class="level"> 75% </div> </div> </li>
+				<li> php 7 <div class="myProgress"> <div class="level"> 60% </div> </div> </li>
+				<li> git <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
+			</ul>
+		</div>
+
+		<div class="frameworks">
+			<ul class="framework">
+				<li> bootstrap <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
+				<li> sass <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
+				<li> jquery <div class="myProgress"> <div class="level"> 80% </div> </div> </li>
+				<li> angularjs <div class="myProgress"> <div class="level"> 70% </div> </div> </li>
+				<li> sql <div class="myProgress"> <div class="level"> 60% </div> </div> </li>
+			</ul>
+		</div>
+	</div>
 
 	<h3> Compétences annexes : </h3>
 	<ul class="annex">
 		<li> ui / ux </li>
 		<li> uml </li>
 		<li> wordpress </li>
+		<li> méthode agile </li>
 	</ul>
 </section>

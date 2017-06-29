20fdd3c79d27f811f3977959a1f008eb8afa9036
abdoulaye <abdoulayedabo@laposte.net>
2017-06-29 23:40:51 +0200

    code css des hobbies, du portfolio et du formulaire de contact

--- views/form.php
+++ views/form.php
@@ -1,30 +1,30 @@
-<section id="form">
+<section id="form" class="form-container">
 	<form action="index.php" method="post">
 		<h1> Me contacter </h1>
 		<label for="lastname"> Nom : </label>
 		<input type="text" name="lastname" id="lastname" required>
 		<span class="error"> Au moins 2 lettres </span>
-		<span class="ok"> Nom valide </span>
+		<span class="success"> Nom valide </span>
 
 		<label for="firstname"> Prénom : </label>
 		<input type="text" name="firstname" id="firstname" required>
 		<span class="error"> Au moins 2 lettres </span>
-		<span class="ok"> Prénom valide </span>
+		<span class="success"> Prénom valide </span>
 
 		<label for="mail"> Adresse mail : </label>
 		<input type="email" name="mail" id="mail" required>
 		<span class="error"> L'adresse mail doit contenir un @ et un . </span>
-		<span class="ok"> Adresse mail valide </span>
+		<span class="success"> Adresse mail valide </span>
 
 		<label for="subject"> Sujet : </label>
 		<input type="text" name="subject" id="subject" required>
 		<span class="error"> Sujet manquant </span>
-		<span class="ok"> Sujet rempli </span>
+		<span class="success"> Sujet rempli </span>
 
 		<label for="message"> Message : </label>
 		<textarea name="message" id="message" required></textarea>
 		<span class="error"> Message vide </span>
-		<span class="ok"> Message rempli </span>
+		<span class="success"> Message rempli </span>
 
 		<button type="submit" id="submitForm"> Envoyer </button>
 		<button type="reset" id="resetForm"> Tout effacer </button>
--- views/portfolio.php
+++ views/portfolio.php
@@ -1,15 +1,15 @@
-<section id="portfolio">
-	<div>
-		<h4> Place des Clichés </h4>
-	</div>
-	<div class="description">
-		<p> Projet client </p>
+<section id="portfolio" class="realisations">
+	<div id="site1" class="realisation">
+		<div class="description">
+			<h4> Place des Clichés </h4>
+			<p> Projet client </p>
+		</div>
 	</div>
 
-	<div>
-		<h4> Mini-CMS de mi-formation en PHP et MySQL </h4>
-	</div>
-	<div class="description">
-		<p> Projet de mi-formation </p>
+	<div id="site2" class="realisation">
+		<div class="description">
+			<h4> Mini-CMS de mi-formation en PHP et MySQL </h4>
+			<p> Projet de mi-formation </p>
+		</div>
 	</div>
 </section>

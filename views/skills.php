e88103f8c460771356e99cea7dbd2391f623461f
abdoulaye <abdoulayedabo@laposte.net>
2017-07-06 23:47:49 +0200

    animation des barres statistiques en css avec @keyframes

--- views/skills.php
+++ views/skills.php
@@ -3,21 +3,21 @@
 	<div class="skills">
 		<div class="languages">
 			<ul class="technical">
-				<li> html 5 <div class="myProgress"> <div class="level"> 90% </div> </div> </li>
-				<li> css 3 <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
-				<li> javascript <div class="myProgress"> <div class="level"> 75% </div> </div> </li>
-				<li> php 7 <div class="myProgress"> <div class="level"> 60% </div> </div> </li>
-				<li> git <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
+				<li> html 5 <div class="myProgress"> <div class="level level90" id="html"> 90% </div> </div> </li>
+				<li> css 3 <div class="myProgress"> <div class="level level85" id="css"> 85% </div> </div> </li>
+				<li> javascript <div class="myProgress"> <div class="level level75" id="js"> 75% </div> </div> </li>
+				<li> php 7 <div class="myProgress"> <div class="level level60" id="php"> 60% </div> </div> </li>
+				<li> git <div class="myProgress"> <div class="level level85" id="git"> 85% </div> </div> </li>
 			</ul>
 		</div>
 
 		<div class="frameworks">
 			<ul class="framework">
-				<li> bootstrap <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
-				<li> sass <div class="myProgress"> <div class="level"> 85% </div> </div> </li>
-				<li> jquery <div class="myProgress"> <div class="level"> 80% </div> </div> </li>
-				<li> angularjs <div class="myProgress"> <div class="level"> 70% </div> </div> </li>
-				<li> sql <div class="myProgress"> <div class="level"> 60% </div> </div> </li>
+				<li> bootstrap <div class="myProgress"> <div class="level level85" id="bootstrap"> 85% </div> </div> </li>
+				<li> sass <div class="myProgress"> <div class="level level85" id="sass"> 85% </div> </div> </li>
+				<li> jquery <div class="myProgress"> <div class="level level80" id="sass"> 80% </div> </div> </li>
+				<li> angularjs <div class="myProgress"> <div class="level level70" id="angularjs"> 70% </div> </div> </li>
+				<li> sql <div class="myProgress"> <div class="level level60" id="sql"> 60% </div> </div> </li>
 			</ul>
 		</div>
 	</div>

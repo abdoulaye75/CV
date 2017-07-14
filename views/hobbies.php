84eee367dd9de3e0400a181f7c66256bb062c679
abdoulaye <abdoulayedabo@laposte.net>
2017-07-14 23:26:52 +0200

    Ajout d'une image dans les hobbies. Liste à puce normal. Pseudo-élément selection stylisé

--- views/hobbies.php
+++ views/hobbies.php
@@ -1,4 +1,7 @@
 <section class="hobbies">
 	<h2> centres d'intérêt </h2>
 	<p> Pratique du fitness 4 fois par semaine </p>
+	<div class="img-fitness">
+		<img src="img/fitness.jpg" alt="fitness">
+	</div>
 </section>

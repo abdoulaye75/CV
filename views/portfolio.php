d3592b7f318ee6daf20c5e6bf4a6d4f3a91b9eb7
abdoulaye <abdoulayedabo@laposte.net>
2017-07-16 16:20:54 +0200

    changement du code html et css du portfolio. Code jquery commenté pour le désactiver

--- views/portfolio.php
+++ views/portfolio.php
@@ -1,13 +1,23 @@
 <section id="portfolio" class="realisations">
-	<div id="site1" class="realisation"></div>
-	<div class="description" id="description1">
-		<h4> Place des Clichés </h4>
-		<p> Projet client </p>
+	<div id="site1" class="realisation">
+		<a href="#">
+			<img src="img/place_des_cliches.png" alt="place des cliches">
+		</a>
+
+		<div class="description" id="description1">
+			<h4> Place des Clichés </h4>
+			<p> Projet client </p>
+		</div>
 	</div>
 
-	<div id="site2" class="realisation"></div>
-	<div class="description" id="description2">
-		<h4> Mini-CMS de mi-formation en PHP et MySQL </h4>
-		<p> Projet de mi-formation </p>
+	<div id="site2" class="realisation">
+		<a href="#">
+			<img src="img/cms.png" alt="Mini-CMS">
+		</a>
+
+		<div class="description" id="description2">
+			<h4> Mini-CMS de mi-formation en PHP et MySQL </h4>
+			<p> Projet de mi-formation </p>
+		</div>
 	</div>
 </section>

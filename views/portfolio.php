d55858d9a42942be0cd494eec7fccfae91de6328
abdoulaye <abdoulayedabo@laposte.net>
2017-07-16 17:39:01 +0200

    ajout de réalisations dans la partie portfolio et de captures d'écran

--- views/portfolio.php
+++ views/portfolio.php
@@ -20,4 +20,92 @@
 			<p> Projet de mi-formation </p>
 		</div>
 	</div>
+
+	<div id="site3" class="realisation">
+		<a href="#">
+			<img src="img/bombe.png" alt="jeu de la bombe en js">
+		</a>
+
+		<div class="description" id="description3">
+			<h4> Bombe à attraper </h4>
+			<p> Jeu de la bombe en JavaScript </p>
+		</div>
+	</div>
+
+	<div id="site4" class="realisation">
+		<a href="#">
+			<img src="img/chronometre.png" alt="chronometre en js">
+		</a>
+
+		<div class="description" id="description4">
+			<h4> Chronomètre en JavaScript </h4>
+			<p> Chronomètre en JavaScript </p>
+		</div>
+	</div>
+
+	<div id="site5" class="realisation">
+		<a href="#">
+			<img src="img/minuteur.png" alt="minuteur en js">
+		</a>
+
+		<div class="description" id="description5">
+			<h4> Minuteur en JavaScript </h4>
+			<p> Minuteur en JavaScript </p>
+		</div>
+	</div>
+
+	<div id="site6" class="realisation">
+		<a href="#">
+			<img src="img/juste-prix.png" alt="juste prix en js">
+		</a>
+
+		<div class="description" id="description6">
+			<h4> Juste prix en JavaScript </h4>
+			<p> Juste prix en JavaScript </p>
+		</div>
+	</div>
+
+	<div id="site7" class="realisation">
+		<a href="#">
+			<img src="img/form.png" alt="formulaire de style en jquery">
+		</a>
+
+		<div class="description" id="description7">
+			<h4> Formulaire de style en jquery </h4>
+			<p> Formulaire de style en jquery </p>
+		</div>
+	</div>
+
+	<div id="site8" class="realisation">
+		<a href="#">
+			<img src="img/tableau_angularjs.png" alt="tableau en angularjs">
+		</a>
+
+		<div class="description" id="description8">
+			<h4> Tableau en angularjs </h4>
+			<p> Tableau en angularjs </p>
+		</div>
+	</div>
+
+	<div id="site9" class="realisation">
+		<a href="#">
+			<img src="img/mini-boutique.png" alt="mini-boutique en angularjs">
+		</a>
+
+		<div class="description" id="description9">
+			<h4> Mini-boutique en angularjs </h4>
+			<p> Mini-boutique en angularjs </p>
+		</div>
+	</div>
+
+	<div id="site10" class="realisation">
+		<a href="#">
+			<img src="img/e-commerce.png" alt="mini-boutique en php/mysql">
+		</a>
+
+		<div class="description" id="description10">
+			<h4> Mini-boutique en php / mysql </h4>
+			<p> Mini-boutique en php / mysql </p>
+		</div>
+	</div>
 </section>
